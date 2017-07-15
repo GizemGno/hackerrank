6cf8e79b9c0e510ca49b916c6875a268d649c1f1
GizemGno <ozdemirgizemnur08@gmail.com>
2017-07-15 05:56:43 +0300

    Simple Algorithm Problem

---
+++ Staircase/soulution.java
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int k=0; k<n-i; k++)
+            cout << " ";
+        for(int j=0; j<i;j++)
+            cout << "#";
+        cout << endl;
+    }
+    return 0;
+}
